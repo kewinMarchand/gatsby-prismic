eb77d6c4d1710757f4b24a954db368e4d408130d
kewinMarchand <k.marchand@purjus.fr>
2019-12-09 00:53:10 +0100

    sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,70 +1,77 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Prismic`,
-    description: `Projet React basé sur le CMS Prismic et le générateur Gatsby.`,
-    author: `Kewin Marchand`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-        resolve: `gatsby-plugin-material-ui`,
-        options: {
-          disableAutoprefixing: false,
-          disableMinification: false,
+    siteMetadata: {
+        title: `Gatsby Prismic`,
+        description: `Projet React basé sur le CMS Prismic et le générateur Gatsby.`,
+        author: `Kewin Marchand`,
+        siteUrl: `https://friendly-minsky-12bb22.netlify.com/`,
+    },
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-sitemap`,
+            options: {
+                sitemapSize: 5000
+            }
         },
-    },
-    {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-            name: `images`,
-            path: `${__dirname}/src/images`,
+        {
+            resolve: `gatsby-plugin-material-ui`,
+            options: {
+                disableAutoprefixing: false,
+                disableMinification: false,
+            },
         },
-    },
-    {
-        resolve: 'gatsby-source-prismic-graphql',
-        options: {
-            repositoryName: 'gatsby-prismic-cms-test', // (REQUIRED, replace with your own prismic repo name)
-            //accessToken: '##########', // (optional API access token)
-            path: '/preview', // (optional preview path. Default: /preview)
-            previews: true, // (optional, activated Previews. Default: false)
-            pages: [
-            { 
-                type: 'Page',         // TypeName from prismic
-                match: '/:uid',  // Pages will be generated under this pattern
-                path: '/',        // Placeholder page for unpublished documents
-                component: require.resolve('./src/templates/page.js'),
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
             },
-            {
-                type: 'Article',
-                match: '/article/:id',
-                path: '/article',
-                component: require.resolve('./src/templates/article.js'),
+        },
+        {
+            resolve: 'gatsby-source-prismic-graphql',
+            options: {
+                //accessToken: '##########', // (optional API access token)
+                repositoryName: 'gatsby-prismic-cms-test', // (REQUIRED, replace with your own prismic repo name)
+                path: '/preview', // (optional preview path. Default: /preview)
+                previews: true, // (optional, activated Previews. Default: false)
+                pages: [
+                { 
+                    type: 'Page',         // TypeName from prismic
+                    match: '/:uid',  // Pages will be generated under this pattern
+                    path: '/',        // Placeholder page for unpublished documents
+                    component: require.resolve('./src/templates/page.js'),
+                },
+                {
+                    type: 'Article',
+                    match: '/article/:id',
+                    path: '/article',
+                    component: require.resolve('./src/templates/article.js'),
+                },
+                {
+                    type: 'Annexe',
+                    match: '/annexe/:id',
+                    path: '/annexe',
+                    component: require.resolve('./src/templates/annexe.js'),
+                },
+                ],
+            }
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
             },
-            {
-                type: 'Annexe',
-                match: '/annexe/:id',
-                path: '/annexe',
-                component: require.resolve('./src/templates/annexe.js'),
-            },
-            ],
-        }
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
 }
