058c7bce9b7d338a60762da7990eeac5baf4baad
kewinMarchand <k.marchand@purjus.fr>
2019-12-08 22:27:29 +0100

    gatsby-plugin-material-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,40 +9,47 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        resolve: `gatsby-plugin-material-ui`,
+        options: {
+          disableAutoprefixing: false,
+          disableMinification: false,
+        },
     },
     {
-      resolve: 'gatsby-source-prismic-graphql',
-      options: {
-        repositoryName: 'gatsby-prismic-cms-test', // (REQUIRED, replace with your own prismic repo name)
-        //accessToken: '##########', // (optional API access token)
-        path: '/preview', // (optional preview path. Default: /preview)
-        previews: true, // (optional, activated Previews. Default: false)
-        pages: [
-          { 
-            type: 'Page',         // TypeName from prismic
-            match: '/:uid',  // Pages will be generated under this pattern
-            path: '/',        // Placeholder page for unpublished documents
-            component: require.resolve('./src/templates/page.js'),
-          },
-          {
-            type: 'Article',
-            match: '/article/:id',
-            path: '/article',
-            component: require.resolve('./src/templates/article.js'),
-          },
-          {
-            type: 'Annexe',
-            match: '/annexe/:id',
-            path: '/annexe',
-            component: require.resolve('./src/templates/annexe.js'),
-          },
-        ],
-      }
+        resolve: `gatsby-source-filesystem`,
+        options: {
+            name: `images`,
+            path: `${__dirname}/src/images`,
+        },
+    },
+    {
+        resolve: 'gatsby-source-prismic-graphql',
+        options: {
+            repositoryName: 'gatsby-prismic-cms-test', // (REQUIRED, replace with your own prismic repo name)
+            //accessToken: '##########', // (optional API access token)
+            path: '/preview', // (optional preview path. Default: /preview)
+            previews: true, // (optional, activated Previews. Default: false)
+            pages: [
+            { 
+                type: 'Page',         // TypeName from prismic
+                match: '/:uid',  // Pages will be generated under this pattern
+                path: '/',        // Placeholder page for unpublished documents
+                component: require.resolve('./src/templates/page.js'),
+            },
+            {
+                type: 'Article',
+                match: '/article/:id',
+                path: '/article',
+                component: require.resolve('./src/templates/article.js'),
+            },
+            {
+                type: 'Annexe',
+                match: '/annexe/:id',
+                path: '/annexe',
+                component: require.resolve('./src/templates/annexe.js'),
+            },
+            ],
+        }
     },
     {
       resolve: `gatsby-plugin-manifest`,
